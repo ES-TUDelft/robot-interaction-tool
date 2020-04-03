5d57bb2ec6ad2ee37c4659e758fe588da0ea0270
Elie <e.saad@tudelft.nl>
2020-04-03 10:13:01 +0200

    Update server.js

--- es_common/server.js
+++ es_common/server.js
@@ -4,3 +4,4 @@ http.createServer(function (req, res) {
   res.write("http://" + req.headers.host + req.url);
   res.end();
 }).listen(8080);
+console.log("Successfully started the server.\nListening at: http://localhost:8080\nUse Ctrl+C to exit.");
