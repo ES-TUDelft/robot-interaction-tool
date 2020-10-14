2b00c74971c871b6851359aa0a8c1252a3bbe977
Elie <e.saad@tudelft.nl>
2020-10-14 09:28:35 +0200

    Delete all files

--- robot_manager/pepper/hre_dialog/html/js/displayimage.js
+++
@@ -1,6 +0,0 @@
-function displayPageImage() {
-    imageName = localStorage.getItem("pageImage");
-    if (imageName) {
-        document.getElementById("pageImage").src = "../pics/" + localStorage.getItem("pageImage");
-    }
-}
